5cf661a6f461eb093df1a58bca0177e6d8cd5a69
Michalis Kargakis <kargakis@protonmail.com>
2021-12-28 11:56:56 +0100

    Test updates

--- test/index.ts
+++ test/index.ts
@@ -21,10 +21,20 @@ export function getFactory<T>(name: string): Promise<T> {
 }
 
 describe("PoisonPill", function () {
-  let signers: SignerWithAddress[];
+  let deployer: SignerWithAddress;
+  let treasury: SignerWithAddress;
+  let trustedUser: SignerWithAddress;
+  let untrustedUser: SignerWithAddress;
+
   before(async () => {
-    signers = await ethers.getSigners();
+    [deployer, treasury, trustedUser, untrustedUser] =
+      await ethers.getSigners();
   });
+
+  const oracleDecimals = 8;
+  const tokenDecimals = 18;
+  const usdcDecimals = 6;
+  const wethDecimals = 18;
 
   let usdc: USDC;
   let weth: WETH;
@@ -37,38 +47,50 @@ describe("PoisonPill", function () {
     it("should properly deploy mocks", async function () {
       usdc = await (await getFactory<USDC__factory>("USDC")).deploy();
       await usdc.deployed();
-      console.log(`USDC: ${usdc.address}`);
 
       weth = await (await getFactory<WETH__factory>("WETH")).deploy();
       await weth.deployed();
-      console.log(`WETH: ${weth.address}`);
 
       wethOracle = await (
         await getFactory<TestPriceOracle__factory>("TestPriceOracle")
-      ).deploy(4100, 8);
+      ).deploy(4100, oracleDecimals);
       await wethOracle.deployed();
-      console.log(`WETH oracle: ${wethOracle.address}`);
 
       token = await (
         await getFactory<TestToken__factory>("TestToken")
-      ).deploy(18);
+      ).deploy(tokenDecimals);
       await token.deployed();
-      console.log(`Token: ${token.address}`);
 
       tokenOracle = await (
         await getFactory<TestPriceOracle__factory>("TestPriceOracle")
-      ).deploy(100, 8);
+      ).deploy(100, oracleDecimals);
       await tokenOracle.deployed();
-      console.log(`Token oracle: ${tokenOracle.address}`);
+    });
+
+    it("should properly prime user balances", async function () {
+      await token
+        .connect(deployer)
+        .mint(treasury.address, 1000000 * tokenDecimals);
+      expect(await token.balanceOf(treasury.address)).to.equal(
+        1000000 * tokenDecimals
+      );
+
+      await usdc
+        .connect(deployer)
+        .mint(trustedUser.address, 2000 * usdcDecimals);
+      expect(await usdc.balanceOf(trustedUser.address)).to.equal(
+        2000 * usdcDecimals
+      );
+
+      await usdc
+        .connect(deployer)
+        .mint(untrustedUser.address, 2000 * usdcDecimals);
+      expect(await usdc.balanceOf(untrustedUser.address)).to.equal(
+        2000 * usdcDecimals
+      );
     });
 
     it("should properly deploy the poison pill", async function () {
-      console.log(`USDC: ${usdc.address}`);
-      console.log(`WETH: ${weth.address}`);
-      console.log(`WETH oracle: ${wethOracle.address}`);
-      console.log(`Token: ${token.address}`);
-      console.log(`Token oracle: ${tokenOracle.address}`);
-
       poisonPill = await (
         await getFactory<PoisonPill__factory>("PoisonPill")
       ).deploy(
@@ -77,27 +99,12 @@ describe("PoisonPill", function () {
         token.address,
         wethOracle.address,
         tokenOracle.address,
-        signers[1].address,
-        [],
+        treasury.address,
+        [trustedUser.address],
         0,
         1000
       );
       await poisonPill.deployed();
     });
   });
-
-  // it("should deploy mock contracts", async function () {
-  //   const PoisonPill = await ethers.getContractFactory("PoisonPill");
-  //   const pp = await PoisonPill.deploy("Hello, world!");
-  //   await pp.deployed();
-
-  //   expect(await greeter.greet()).to.equal("Hello, world!");
-
-  //   const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-  //   // wait until the transaction is mined
-  //   await setGreetingTx.wait();
-
-  //   expect(await greeter.greet()).to.equal("Hola, mundo!");
-  // });
 });
